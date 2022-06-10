f36d02471a6e847202078548a1d8607583321e8b
Borkeyda <borisencinas22@gmail.com>
2022-06-10 12:58:41 -0400

    pagina under terminada

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Prodnike/components/App';
 import App from './Produnder/components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
